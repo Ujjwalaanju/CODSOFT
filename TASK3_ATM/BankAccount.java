823234d4b7250e1963d8c838762c5560c619cf89
Ujjwalaanju <ujjwalaanjuakuluru@gmail.com>
2026-02-28 10:38:39 +0530

    Add files via upload

---
+++ TASK3_ATM/BankAccount.java
@@ -0,0 +1,35 @@
+
+public class BankAccount {
+
+
+    private double balance;
+
+    public BankAccount(double initialBalance) {
+        this.balance = initialBalance;
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Deposit successful!");
+        } else {
+            System.out.println("Invalid deposit amount.");
+        }
+    }
+
+    public boolean withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+            System.out.println("Withdrawal successful!");
+            return true;
+        } else {
+            System.out.println("Insufficient balance or invalid amount.");
+            return false;
+        }
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+}
+
